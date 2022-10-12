05feb66cf0d39689077a3235f6f3d3cfb0c1a7c8
tupiznak <akej-vonavi@mail.ru>
2022-10-12 09:25:04 +0000

    init

---
+++ ml_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Homework MADE 2022 MLOps',
+    author='tupiznak',
+    license='',
+)
