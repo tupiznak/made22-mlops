1a26da0fe93ac29d68963911d78c68b27b1fc898
tupiznak <akej-vonavi@mail.ru>
2022-10-12 11:28:22 +0000

    rename, remove docs

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='mlops-homework',
     packages=find_packages(),
     version='0.1.0',
     description='Homework MADE 2022 MLOps',
