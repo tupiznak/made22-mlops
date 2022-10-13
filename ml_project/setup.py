bcb8cd4fdbe17ba59eb8c7ccabdf2ae8a280200e
tupiznak <akej-vonavi@mail.ru>
2022-10-13 14:50:14 +0000

    rename

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mlops-homework',
+    name='mlops_homework',
     packages=find_packages(),
     version='0.1.0',
     description='Homework MADE 2022 MLOps',
     author='tupiznak',
     license='',
+    entry_points={
+        "console_scripts": [
+            "mlops_homework_download_dataset = mlops_homework.data.load_dataset:main"
+        ]
+    },
 )
