c8607ca6c95a93bc71a3e36ef5a99abd301eb211
tupiznak <akej-vonavi@mail.ru>
2022-10-15 16:14:22 +0000

    try merge dvc hydra

--- ml_project/mlops_homework/models/train_model.py
+++ ml_project/mlops_homework/models/train_model.py
@@ -3,8 +3,13 @@ import hydra
 from mlops_homework.conf.config import Config
 from .baseline.train_baseline_model import train_model
 
+import dvc.api
+
 
 @hydra.main(version_base=None, config_path='../conf', config_name="config")
 def main(cfg: Config):
+    dvc_params = dvc.api.params_show()
+    [setattr(cfg, k, v) for k, v in dvc_params.items()]
+    print(cfg)
     if cfg.model.name == 'baseline':
         train_model(test_split_size=cfg.model.test_split_size, random_state=cfg.random_state)
