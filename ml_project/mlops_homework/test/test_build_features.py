0f9b07e823fe1a482ffc82b0ef9696742a329c1a
tupiznak <akej-vonavi@mail.ru>
2022-11-13 16:50:14 +0000

    add faker

--- ml_project/mlops_homework/test/test_build_features.py
+++ ml_project/mlops_homework/test/test_build_features.py
@@ -1,3 +1,4 @@
+from faker import Faker
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import scale
@@ -6,14 +7,23 @@ from mlops_homework.features.build_features import DataTransformer
 
 
 def test_transform(input_data):
+    fake = Faker()
     transform = DataTransformer()
+
     x_input = input_data[0]
+    ages = [fake.random_int(min=10, max=90) for _ in range(x_input.shape[0])]
+    x_input.iloc[:, 0] = ages
+
     transform.fit(x_input, ['sex'])
     x = x_input.to_numpy()
+
+    size = 20
+    start_idx = 3
+    test_data = x[start_idx:start_idx + size]
     need_data = pd.concat((x_input.drop(['sex'], axis=1),
                            pd.DataFrame(data=x_input['sex'] == 0),
                            pd.DataFrame(data=x_input['sex'] == 1)
                            ), axis=1).to_numpy()
-    assert np.all(transform.transform_categorical(x[3:5]) == need_data[3:5])
+    assert np.all(transform.transform_categorical(test_data) == need_data[start_idx:start_idx + size])
     need_data = scale(need_data)
-    assert np.all(np.isclose(transform.transform(x[3:5]), need_data[3:5]))
+    assert np.all(np.isclose(transform.transform(test_data), need_data[start_idx:start_idx + size]))
