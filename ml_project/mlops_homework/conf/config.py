404c43136b2d19b01591c8aa31c87fad869a2b81
tupiznak <akej-vonavi@mail.ru>
2022-11-13 14:23:13 +0000

    upd eda config

--- ml_project/mlops_homework/conf/config.py
+++ ml_project/mlops_homework/conf/config.py
@@ -1,6 +1,11 @@
+import os
 from dataclasses import dataclass
 
 from hydra.core.config_store import ConfigStore
+
+PROJECT_PATH = os.getenv('PROJECT_PATH', None)
+if PROJECT_PATH is None:
+    raise ModuleNotFoundError('need set PROJECT_PATH env')
 
 
 @dataclass
@@ -24,6 +29,8 @@ class Config:
     model: ModelConfig
     preprocessing: PreprocessingConfig
     random_state: int
+    relative_path_to_data_storage: str
+    relative_path_to_reports_storage: str
 
 
 ConfigStore.instance().store(name="base_config", node=Config)
--- ml_project/mlops_homework/data/make_eda.py
+++ ml_project/mlops_homework/data/make_eda.py
@@ -1,7 +1,16 @@
+import hydra
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-if __name__ == '__main__':
-    df = pd.read_csv("../../data/raw/heart_cleveland_upload.csv")
+from mlops_homework.conf.config import PROJECT_PATH, Config
+
+
+@hydra.main(version_base=None, config_path='../conf', config_name="config")
+def main(cfg: Config):
+    df = pd.read_csv(PROJECT_PATH + cfg.relative_path_to_data_storage)
     profile = ProfileReport(df, title="EDA Report")
-    profile.to_file('../../reports/EDA.html')
+    profile.to_file(PROJECT_PATH + cfg.relative_path_to_reports_storage)
+
+
+if __name__ == '__main__':
+    main()
