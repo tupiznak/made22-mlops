f955e132c121c38490617b52b3262ad43194b957
tupiznak <akej-vonavi@mail.ru>
2022-11-14 13:02:30 +0000

    made struct + base

---
+++ online_inference/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlops_homework_server',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Homework 2 MADE 2022 MLOps',
+    author='tupiznak',
+    license='',
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
